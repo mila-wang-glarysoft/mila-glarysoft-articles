be5929a0aa1f2af89883eac4b27f1cfbf9569ceb
Michael Song <michaelsong@glarysoft.com>
2026-06-14 17:37:29 +0800

    Prevent indexing on Pages preview domain

---
+++ functions/_middleware.js
@@ -0,0 +1,28 @@
+const BLOCKED_HOSTS = new Set(['glarysoft-blog.pages.dev']);
+
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+
+  if (!BLOCKED_HOSTS.has(url.hostname)) {
+    return context.next();
+  }
+
+  if (url.pathname === '/robots.txt') {
+    return new Response('User-agent: *\nDisallow: /\n', {
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+        'X-Robots-Tag': 'noindex, nofollow',
+      },
+    });
+  }
+
+  const response = await context.next();
+  const headers = new Headers(response.headers);
+  headers.set('X-Robots-Tag', 'noindex, nofollow');
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers,
+  });
+}
